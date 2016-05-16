2200a622673aa50cd17b5192cdba129a73fdf4e6
raulovidiu <raulhuluban@gmail.com>
2016-05-16 21:16:25 +0300

    navigate to /login route

--- signIn.js
+++ signIn.js
@@ -1,6 +1,8 @@
 'use strict';
 
 describe('Authenticate a Valid User', function() {
+    var loginBtn = element(by.css('a.header-link-login.text-uppercase'));
+
 
     it('Should Navigate to Home Page', function() {
         browser.driver.manage().window().maximize();
@@ -9,4 +11,15 @@ describe('Authenticate a Valid User', function() {
         expect(becomeFreelancer.isPresent()).toBe(true);
     });
 
+
+    it('Should Click the Login Button', function() {
+        loginBtn.click();
+        expect(browser.getCurrentUrl()).toBe('https://www.upwork.com/login');
+    });
+
+
 });
+
+
+// click login button -> navigate to /login -> assert
+//
