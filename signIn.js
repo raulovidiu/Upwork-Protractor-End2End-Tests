5fd6bc0b95c4a96c47946c2c35028885ca1c1d35
raulovidiu <raulhuluban@gmail.com>
2016-05-16 21:37:22 +0300

    implemented the login test and the assertion

--- signIn.js
+++ signIn.js
@@ -2,7 +2,9 @@
 
 describe('Authenticate a Valid User', function() {
     var loginBtn = element(by.css('a.header-link-login.text-uppercase'));
-
+    var userName = element(by.model('username'));
+    var password = element(by.model('password'));
+    var logInBtn = element(by.buttonText('Log In'));
 
     it('Should Navigate to Home Page', function() {
         browser.driver.manage().window().maximize();
@@ -18,8 +20,12 @@ describe('Authenticate a Valid User', function() {
     });
 
 
+    it('Should Enter Valid Credentials and Click Log In', function() {
+        userName.click().sendKeys('RamonGuiCosta2@yahoo.es');
+        password.click().sendKeys('Password1!$');
+        logInBtn.click();
+        expect(browser.getCurrentUrl()).toBe('https://www.upwork.com/ab/jobs-home/2988154?cell=Treatment3');
+    });
+
+
 });
-
-
-// click login button -> navigate to /login -> assert
-//
